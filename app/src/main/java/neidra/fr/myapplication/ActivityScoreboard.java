702a8d60b25819afe772eb852d77769461bc995a
Adrien222 <a.chretien@etud.u-picardie.fr>
2022-02-28 17:48:19 +0100

    Modif nom bouton page accueil + modif nom classe et activity

--- app/src/main/java/neidra/fr/myapplication/ActivityScoreboard.java
+++ app/src/main/java/neidra/fr/myapplication/ActivityScoreboard.java
@@ -1,14 +1,13 @@
 package neidra.fr.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class ActivityNiveau2 extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class ActivityScoreboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_niveau2);
+        setContentView(R.layout.activity_scoreboard);
     }
 }
